5e95b8adb11973549e738f65c145592c752e504f
Cenk Korkmaz <Cenk.Korkmaz92@gmail.com>
2025-07-01 17:06:08 +0200

    Added pagination to API responses

--- coderr_backend/settings.py
+++ coderr_backend/settings.py
@@ -101,6 +101,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',  # Safe default
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
 }
 
 CORS_ALLOWED_ORIGINS = [
